90993f44eec7f0fc8be0c48112d9c7f07120e0ea
Hamed12142212 <155337272+Hamed12142212@users.noreply.github.com>
2024-12-13 07:33:43 +0200

    Update main.js

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ const map = new mapboxgl.Map({
     pitch: 45,
     bearing: 90,
     doubleClickZoom: false
-    map.scrollZoom.disable();
+    scrollZoom: false
 });
 
 let currentView = 'building';
